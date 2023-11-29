72dd25a88031851316ced11718f363fb72933c41
Erik Isberg <erik@isberg.nu>
2023-11-29 10:47:11 +0100

    Update funktion.js

--- funktion.js
+++ funktion.js
@@ -1,10 +1,11 @@
 (function() {
     const widgetHTML = `
         <div id="charity-widget" style="position: fixed; bottom: 10px; left: 10px; z-index: 1000;">
-            <img src="https://uploads-ssl.webflow.com/5d9cc2c7b5891d21c8901fd2/6566f5a08416eed5e53cc30e_MH%20Stickers%20Vi%20stodjer%20-%20original%20(957673).png" style="width: 100px; height: 100px; cursor: pointer;">
+            <img src="https://uploads-ssl.webflow.com/5d9cc2c7b5891d21c8901fd2/5df520e1b0ebe722d03f9f82_dios_logo_cmyk_vit.png" style="width: 100px; height: 100px; cursor: pointer;">
         </div>
         <div id="charity-overlay" style="display:none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background: rgba(0, 0, 0, 0.5); z-index: 1001;">
             <div id="charity-popup" style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background: white; max-width: 600px; padding: 20px; font-family: 'Open Sans', sans-serif;">
+                <span id="close-popup" style="position: absolute; top: 10px; right: 10px; cursor: pointer;">Close</span>
                 <img src="https://uploads-ssl.webflow.com/5d9cc2c7b5891d21c8901fd2/6565dfca12f98697371601ea_download-1.webp" style="width: 50%;">
                 <h1>Musikhjälpen 2023</h1>
                 <p>Mat är en mänsklig rättighet. Trots det dör människor runtom i världen varje dag till följd av undernäring. Omkring var tionde människa vet inte när eller vad de kommer att äta nästa gång. Genom Musikhjälpen 2023 kan alla vara med och bidra till kampen mot hungern.
